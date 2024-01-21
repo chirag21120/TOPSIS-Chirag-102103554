5ab02a85c90ce47ea8192caf2b1f1e90cdc0a78f
CHIRAG <cgupta_be21@thapar.edu>
2024-01-22 00:42:57 +0530

    minor change

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
   keywords = ['TOPSIS', '102103554', 'Chirag Mohan Gupta'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
